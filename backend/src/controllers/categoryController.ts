45a66277743db8b94ca3d0c39d0dc59b31e51b56
harshitclub <harshitclub@gmail.com>
2024-07-20 21:35:56 +0530

    category

--- backend/src/controllers/categoryController.ts
+++ backend/src/controllers/categoryController.ts
@@ -1,11 +1,28 @@
 import { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
-import bcrypt from "bcrypt";
+import { addCatValidator } from "../validator/categoryValidator";
 
 const prisma = new PrismaClient();
 
 export const addCategory = async (req: Request, res: Response) => {
   try {
+    const { title, description, slug, metaDescription, keywords } =
+      await addCatValidator.parseAsync(req.body);
+
+    const addCategory = await prisma.category.create({
+      data: {
+        title,
+        description,
+        slug,
+        metaDescription,
+        keywords,
+      },
+    });
+
+    return res.status(201).json({
+      message: "Category Created",
+      category: addCategory,
+    });
   } catch (error) {
     // @ts-ignore
     console.error(error.message); // Log the error for debugging
